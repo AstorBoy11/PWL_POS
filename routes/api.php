02c199541c4ae56558735d1f5e536aeb23bf058a
arilibbetardanaputra <arilardana111@gmail.com>
2025-04-28 20:12:41 +0700

    API Routing

--- routes/api.php
+++ routes/api.php
@@ -1,66 +1,27 @@
 <?php
+ 
+ use App\Http\Controllers\Api\RegisterController;
+ use Illuminate\Http\Request;
+ use Illuminate\Support\Facades\Route;
+ 
+ /*
+ |--------------------------------------------------------------------------
+ | API Routes
+ |--------------------------------------------------------------------------
+ |
+ | Here is where you can register API routes for your application. These
+ | routes are loaded by the RouteServiceProvider and all of them will
+ | be assigned to the "api" middleware group. Make something great!
+ |
+ */
 
-use App\Http\Controllers\Api\RegisterController;
-use App\Http\Controllers\Api\LoginController;
-use App\Http\Controllers\Api\LogoutController;
-use App\Http\Controllers\Api\LevelController;
-use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\KategoriController;
-use App\Http\Controllers\Api\BarangController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+    Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+    Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+    Route::get('/level', [App\Http\Controllers\Api\LevelController::class, 'index'])->name('level');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+    // Route::get('/user', App\Http\Controllers\Api\UserController::class)->name('user');
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-// route register
-Route::post('/register', RegisterController::class)->name('register'); //[RegisterController::class, 'register']);
-
-// route login
-Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-// Route Logout
-Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
-
-// Route Level
-Route::get('/levels', [LevelController::class, 'index']);
-Route::post('/levels', [LevelController::class, 'store']);
-Route::get('/levels/{level}', [LevelController::class, 'show']);
-Route::put('/levels/{level}', [LevelController::class, 'update']);
-Route::delete('/levels/{level}', [LevelController::class, 'destroy']);
-
-// Routes User
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/users', [UserController::class, 'store']);
-Route::get('/users/{user}', [UserController::class, 'show']);
-Route::put('/users/{user}', [UserController::class, 'update']);
-Route::delete('/users/{user}', [UserController::class, 'destroy']);
-
-// Routes Kategori
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::post('/kategori', [KategoriController::class, 'store']);
-Route::get('/kategori/{kategori}', [KategoriController::class, 'show']);
-Route::put('/kategori/{kategori}', [KategoriController::class, 'update']);
-Route::delete('/kategori/{kategori}', [KategoriController::class, 'destroy']);
-
-// Routes Barang
-Route::get('/barang', [BarangController::class, 'index']);
-Route::post('/barang', [BarangController::class, 'store']);
-Route::get('/barang/{barang}', [BarangController::class, 'show']);
-Route::put('/barang/{barang}', [BarangController::class, 'update']);
-Route::delete('/barang/{barang}', [BarangController::class, 'destroy']);
